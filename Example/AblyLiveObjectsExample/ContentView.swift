cb5700156e5a053158ad6ce860d9ba37a962bdd1
Marat Al <m.alekperov@gmail.com>
2025-08-16 00:35:08 +0200

    Add `attachAsync` extension because the one that exists is in the test target.

--- Example/AblyLiveObjectsExample/ContentView.swift
+++ Example/AblyLiveObjectsExample/ContentView.swift
@@ -400,3 +400,29 @@ struct VoteRow: View {
     
     return PreviewWrapper()
 }
+
+extension ARTRealtimeChannelProtocol {
+    func attachAsync() async throws(ARTErrorInfo) {
+        try await withCheckedContinuation { (continuation: CheckedContinuation<Result<Void, ARTErrorInfo>, _>) in
+            attach { error in
+                if let error {
+                    continuation.resume(returning: .failure(error))
+                } else {
+                    continuation.resume(returning: .success(()))
+                }
+            }
+        }.get()
+    }
+
+    func detachAsync() async throws(ARTErrorInfo) {
+        try await withCheckedContinuation { (continuation: CheckedContinuation<Result<Void, ARTErrorInfo>, _>) in
+            detach { error in
+                if let error {
+                    continuation.resume(returning: .failure(error))
+                } else {
+                    continuation.resume(returning: .success(()))
+                }
+            }
+        }.get()
+    }
+}
